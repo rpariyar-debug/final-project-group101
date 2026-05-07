a0d5f12a29df7852b7e31f9881cde56de4a8ceac
acolli09-stack <acolli09@terpmail.umd.edu>
2026-05-07 17:29:05 -0400

    updated main_game.py

--- main_game.py
+++ main_game.py
@@ -1,5 +1,30 @@
 from guesses import MoveTracker, get_player_guess
+from duplicates import
+from grid_generator import 
+from healthbar import
 
-tracker = MoveTracker(rows=7, columns=7)
+def main():
+    print("Welcome to the Fishing Game!")
+    print("Catch 5 fish before you run out of health, guesses, or time.")
+    print("You have 3 health, 10 guesses, and 2 minutes.\n")
+    
+    grid = 
+    move_tracker = MoveTracker()
+    game_state = 
+    health_bar = 
+    
+    
+    
+move_tracker = MoveTracker(rows=7, columns=7)
 
-row, column = get_player_guess(tracker)
+while True:
+    row, column = get_player_guess(move_tracker)
+    
+    print(f"You selected row {row + 1}, column {column + 1}")
+    
+    print(f"Used moves: {move_tracker.used_moves}")
+    
+    print()
+    
+if __name__ == "__main__":
+    main()
